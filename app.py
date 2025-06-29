af9016a05d26b4faeb548c13b1b4ec18e6b66ccc
mrusercse <mra974836@gmail.com>
2025-06-29 09:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ def analyze_clauses_inline(clauses):
 
 @st.cache_resource
 def load_summarizer():
-    # return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
-    return pipeline("summarization", model="knkarthick/MEETING_SUMMARY", framework="pt")
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
+    # return pipeline("summarization", model="knkarthick/MEETING_SUMMARY", framework="pt")
 
 
 
